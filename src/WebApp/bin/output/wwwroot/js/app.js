52aa85c937df8d29db20cc49484d6aac4efc671d
bipins <bipindra.shrestha@gmail.com>
2015-11-13 14:00:47 -0500

    added angular ui routing

---
+++ src/WebApp/bin/output/wwwroot/js/app.js
@@ -0,0 +1,6 @@
+﻿// Write your Javascript code.
+var app = angular.module('myApp', ['angularCharts']);
+
+window.onerror = function (a, b, c) {
+    alert(a + b + c);
+}
--- src/WebApp/wwwroot/js/app.js
+++ src/WebApp/wwwroot/js/app.js
@@ -1,5 +1,20 @@
 ﻿// Write your Javascript code.
-var app = angular.module('myApp', ['angularCharts']);
+var app = angular.module('myApp', ['ui.router', 'angularCharts'])
+.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+        .state('home', {
+            url: '/home',
+            templateUrl: 'templates/home/index.htm',
+            controller: 'homeController'
+            
+        });
+
+})
+
 
 window.onerror = function (a, b, c) {
     alert(a + b + c);
