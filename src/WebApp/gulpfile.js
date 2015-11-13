28d444178032ac43150e8f9550b761411c0d6e92
bipins <bipindra.shrestha@gmail.com>
2015-11-13 12:13:49 -0500

    initial commit

---
+++ src/WebApp/gulpfile.js
@@ -0,0 +1,65 @@
+﻿/// <binding Clean='clean' />
+
+var gulp = require("gulp"),
+    rimraf = require("rimraf"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    uglify = require("gulp-uglify"),
+    project = require("./project.json");
+
+
+var paths = {
+    webroot: "./" + project.webroot + "/"
+};
+
+paths.js = paths.webroot + "js/**/*.js";
+paths.minJs = paths.webroot + "js/**/*.min.js";
+paths.css = paths.webroot + "css/**/*.css";
+paths.minCss = paths.webroot + "css/**/*.min.css";
+paths.concatJsDest = paths.webroot + "js/app.min.js";
+paths.concatCssDest = paths.webroot + "css/app.min.css";
+
+//gulp.task("default",['clean','min'],function(){
+//    console.log("Hello Default task.");
+//});
+
+var dnx = require("gulp-dnx");
+gulp.task('default', function (cb) {
+
+    return gulp.start('dnx-run');
+});
+
+var options = {
+    restore: true,
+    build: true,
+    run: true,
+    cwd: './'
+};
+var dnxCommand = '. web';
+gulp.task('dnx-run', dnx(dnxCommand, options));
+
+gulp.task("clean:js", function (cb) {
+    rimraf(paths.concatJsDest, cb);
+});
+
+gulp.task("clean:css", function (cb) {
+    rimraf(paths.concatCssDest, cb);
+});
+
+gulp.task("clean", ["clean:js", "clean:css"]);
+
+gulp.task("min:js", function () {
+    gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+        .pipe(concat(paths.concatJsDest))
+        .pipe(uglify())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min:css", function () {
+    gulp.src([paths.css, "!" + paths.minCss])
+        .pipe(concat(paths.concatCssDest))
+        .pipe(cssmin())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min", ["min:js", "min:css"]);
---
+++ src/WebApp/wwwroot/js/services/dataService.js
@@ -0,0 +1,25 @@
+﻿app.factory('dataService', ['$http', '$q', function ($http, $q) {
+
+    function getData(url) {
+        var deferred = $q.defer();
+        var config = {};
+        $http.get(url, config).then(function (response) {
+            deferred.resolve(response.data);
+        }, function (response) {
+            deferred.reject(response.data);
+        });
+        return deferred.promise;
+    }
+
+    function convertToChartData(inputData) {
+        return {
+            data: inputData
+        };
+    }
+    return {
+        getData: getData,
+        convertToChartData: convertToChartData
+    };
+}]);
+
+
