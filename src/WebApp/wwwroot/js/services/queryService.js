d081215ad90064baa138365c9239008c79d7a20a
bshrestha-REISys <bipindra.shrestha@gmail.com>
2015-11-14 15:53:49 -0500

    adding SOQL query generator service and unit tests.

---
+++ src/WebApp/wwwroot/js/services/queryService.js
@@ -0,0 +1,73 @@
+﻿
+app.factory('queryService', function () {
+
+    var getSoQLQueryString = function (queryObj) {
+        var queryResult = "";
+        if (typeof (queryObj) !== "undefined" && queryObj !== null) {
+
+            //Resolve Select
+            var select = "";
+            var defaultSelectParams = [];
+
+            if (queryObj.select && queryObj.select.length > 0) {
+                select = "$select=" + queryObj.select.join(",");
+            } else {
+                select = "$select=" + defaultSelectParams.join(",");
+            }
+
+            queryResult = select;
+
+            //Resolve Where
+            var where = "";
+            var tempWhereArray = [];
+            if (queryObj.where && queryObj.where.length > 0) {
+                for (var i = 0; i < queryObj.where.length; i++) {
+                    if (queryObj.where[i].key && queryObj.where[i].value && queryObj.where[i].operator && queryObj.where[i].key !== "" && queryObj.where[i].value !== "" && queryObj.where[i].operator !== "") {
+                        tempWhereArray.push(queryObj.where[i].key + queryObj.where[i].operator + queryObj.where[i].value);
+                    }
+                }
+
+                if (tempWhereArray.length > 0) {
+                    where = "$where=" + tempWhereArray.join(" AND ");
+                }
+
+                if (where !== "") {
+                    queryResult = queryResult + "&" + where;
+                }
+
+                //Resolve OrderBy
+                var orderByStr = "";
+                if (queryObj.orderBy && queryObj.orderBy.columns && queryObj.orderBy.columns.length > 0) {
+                    orderByStr = queryObj.orderBy.columns.join(",");
+
+                    if (queryObj.orderBy.suffix && queryObj.orderBy.suffix !== "") {
+                        orderByStr = orderByStr + " " + queryObj.orderBy.suffix;
+                    } else {
+                        orderByStr = orderByStr + " " + "ASC";
+                    }
+                }
+
+                if (orderByStr !== "") {
+                    orderByStr = "$order=" + orderByStr;
+                    queryResult = queryResult + "&" + orderByStr;
+                }
+
+                //Resolve GroupBy
+                var groupByStr = "";
+                if (queryObj.groupBy && queryObj.groupBy.length > 0) {
+                    groupByStr = queryObj.groupBy.join(",");
+                }
+
+                if (groupByStr !== "") {
+                    groupByStr = "$group=" + groupByStr;
+                    queryResult = queryResult + "&" + groupByStr;
+                }
+            }
+        }
+        return queryResult;
+    };
+
+    return {
+        getQueryString: getSoQLQueryString
+    };
+})
---
+++ src/WebApp/wwwroot/tests/services/queryService.test.js
@@ -0,0 +1,31 @@
+﻿'use strict'
+describe('query service test', function () {
+    describe('when I call dataService', function () {
+        beforeEach(function () {
+            module('myApp');
+        });
+
+        var def;
+        var $injector;
+        beforeEach(inject(function (_$state_, _$q_, _$templateCache_, _$location_, _$rootScope_, _$injector_) {
+
+            $injector = _$injector_;
+        }));
+
+
+
+        it('returns query string with just one column to select ', function () {
+            var service = $injector.get('queryService');
+            var dummy = {
+                "select": ["applicant_income_000s"],
+                "where": [],
+                "orderBy": {
+                    "columns": [],
+                    "suffix": ""
+                },
+                "groupBy": []
+            };
+            expect(service.getQueryString(dummy)).toEqual('$select=applicant_income_000s');
+        });
+    });
+});
