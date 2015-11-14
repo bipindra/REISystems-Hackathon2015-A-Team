e8f004c39bd7e5028afc6c38aaf25b27774e47cd
bipins <bipindra.shrestha@gmail.com>
2015-11-13 22:01:33 -0500

    Using gulp to run karma

--- src/WebApp/gulpfile.js
+++ src/WebApp/gulpfile.js
@@ -6,7 +6,9 @@ var gulp = require("gulp"),
     cssmin = require("gulp-cssmin"),
     uglify = require("gulp-uglify"),
     project = require("./project.json"),
-    jasmine = require("gulp-jasmine");
+    jasmine = require("gulp-jasmine"),
+    dnx = require("gulp-dnx"),
+    Server = require('karma').Server;
 
 
 var paths = {
@@ -20,11 +22,7 @@ paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/app.min.js";
 paths.concatCssDest = paths.webroot + "css/app.min.css";
 
-//gulp.task("default",['clean','min'],function(){
-//    console.log("Hello Default task.");
-//});
 
-var dnx = require("gulp-dnx");
 gulp.task('default', function (cb) {
 
     return gulp.start('dnx-run');
@@ -65,8 +63,13 @@ gulp.task("min:css", function () {
 
 gulp.task("min", ["min:js", "min:css"]);
 
-gulp.task("test", function () {
-    return gulp.src('wwwroot/tests/test.js')
-        // gulp-jasmine works on filepaths so you can't have any plugins before it 
-        .pipe(jasmine());
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done).start();
 });
