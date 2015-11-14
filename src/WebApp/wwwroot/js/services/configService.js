c9ae23b72f5acbcda92019ea5ec43385623dbd01
ScottKeeler-REISys <scott.keeler@reisystems.com>
2015-11-14 14:58:56 -0500

    Lookup Service and LookupURL in Config Service

--- src/WebApp/wwwroot/js/services/configService.js
+++ src/WebApp/wwwroot/js/services/configService.js
@@ -1,7 +1,7 @@
 app.factory('configService', [function () {
 
     var configurations = [];
-
+    configurations['lookupUrl'] = "https://api.consumerfinance.gov/data/hmda/concept/"
     return {
         getConfig:function(what){
             return configurations[what];
--- src/WebApp/wwwroot/js/services/lookupService.js
+++ src/WebApp/wwwroot/js/services/lookupService.js
@@ -1,6 +1,7 @@
 app.factory('lookupService', ['$q','dataService','configService', function ( $q,dataService,configService) {
     function getLookup(table) {
-
+        var url = configService.getConfig('lookupUrl') + table + ".json";
+        return dataService.getData(url);
     }
 
     return {
