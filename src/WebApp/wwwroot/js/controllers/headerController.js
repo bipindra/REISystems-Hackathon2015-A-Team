b1bcdcc7b5b58d891f02d1655c58a979c24558fe
PrasadPai-REISys <Prasad.Pai@reisys.com>
2015-11-14 17:02:26 -0500

    Adding Header Controller and Related Logic

---
+++ src/WebApp/wwwroot/js/controllers/headerController.js
@@ -0,0 +1,122 @@
+﻿app.controller('headerController', function headerController($scope, lookupService) {
+
+    $scope.SelectedLoanPurposeValue = 'Select Loan Purpose';
+    $scope.SelectedLoanPurposeCode = 0
+
+    
+    $scope.SelectedStateCode = 0;
+    $scope.SelectedStateValue = 'Select State';
+
+    $scope.SelectedCountyCode = 0;
+    $scope.SelectedCountyValue = 'Select County';
+
+    GetLookups()
+
+    function GetLookups() {
+
+        
+
+        lookupService.getLookup('fips').then(function (data) {
+            var results = angular.fromJson(data).table.data;
+            $scope.lookupData = [];
+            for (var i in results) {
+                $scope.lookupData.push({ Code: results[i]._id, Value: results[i].county_name })
+            }
+            
+        });
+       
+        lookupService.getLookup('state_code').then(function (data) {
+            var results = angular.fromJson(data).table.data;
+            var states = [{ Code:0 , Value : 'Select State'}];
+            for (var i in results) {
+                //if (i.length === 1)
+                //{
+                //    debugger;
+                //    results[i]._id = '0' + results[i]._id;
+                //}
+                states.push({ Code: results[i]._id, Value: results[i].name })
+            }
+            $scope.lookupState = states;
+        });
+
+        lookupService.getLookup('loan_purpose').then(function (data) {
+            var lookupResults = angular.fromJson(data).table.data;
+            var newData = [{ Code: 0, Value: 'Select Loan Purpose' }];
+            for (var i in lookupResults) {
+                newData.push({ Code: lookupResults[i]._id, Value: lookupResults[i].name });
+            }
+            $scope.lookupLoanPurpose = newData;
+        });
+
+        //lookupService.getLookup('state_code').then(function (data) {
+        //    var lookupResults = angular.fromJson(data).table.data;
+        //    var newData = [];
+        //    for (var i in lookupResults) {
+        //        newData.push({ Code: lookupResults[i]._id, Value: lookupResults[i].name });
+        //    }
+        //    $scope.lookupLoanPurpose = newData;
+        //});
+    }
+
+    
+
+
+    //$scope.lookupLoanPurpose = 
+    //    [
+    //                    { Code: 0, Value: 'Select Loan Purpose' },
+    //                    { Code: 1, Value: 'Home purchase' },
+    //                    { Code: 2, Value: 'Home improvement' },
+    //                    { Code: 3, Value: 'Refinancing' }
+    //    ];
+    
+                        
+
+    //$scope.lookupState =
+    //    [
+    //                    { Code: 0, Value: 'Select State' },
+    //                    { Code: 1, Value: 'VA' },
+    //                    { Code: 2, Value: 'MD' },
+    //                    { Code: 3, Value: 'DC' }
+    //    ];
+        
+
+    
+
+    $scope.LoanPurposeSelected = function (LoanPurposeCode, LoanPurposeValue) {
+        $scope.SelectedLoanPurposeValue = LoanPurposeValue;
+        $scope.SelectedLoanPurposeCode = LoanPurposeCode;
+    }
+
+    $scope.StateSelected = function (StateCode, StateValue) {
+
+        
+        if (StateCode < 10)
+        {
+            StateCode = '0' + StateCode;
+        }
+
+        $scope.SelectedStateCode = StateCode;
+        $scope.SelectedStateValue = StateValue;
+        $scope.SelectedCountyCode = 0;
+        $scope.SelectedCountyValue = 'Select County';
+
+        var counties = [{ Code: 0, Value: 'Select County' }];
+        for (var index = 0; index < $scope.lookupData.length; index++) {
+            if ($scope.lookupData[index].Code.toString().startsWith(StateCode))
+            { counties.push($scope.lookupData[index]) }
+            $scope.lookupCounty = counties;
+        }
+
+    }
+
+    $scope.CountySelected = function (CountyCode, CountyValue) {
+        $scope.SelectedCountyCode = CountyCode;
+        $scope.SelectedCountyValue = CountyValue;
+    }
+
+    $scope.SearchClicked = function () {
+        alert('Loan Purpose : ' + $scope.SelectedLoanPurposeCode + ', State : ' + $scope.SelectedStateCode + ', County :' + $scope.SelectedCountyCode);
+    }
+
+
+});
