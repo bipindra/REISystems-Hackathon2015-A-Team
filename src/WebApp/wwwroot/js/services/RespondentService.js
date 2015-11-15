de6e1d115e2bae3f8ae0e425951477d357f0f825
PrasadPai-REISys <Prasad.Pai@reisys.com>
2015-11-14 21:06:01 -0500

    Adding respondentService

---
+++ src/WebApp/wwwroot/js/services/RespondentService.js
@@ -0,0 +1,11 @@
+﻿app.factory('respondentService', ['$q', 'dataService', function ($q, dataService) {
+
+    function getRespondents(filter) {
+        var url = configService.getConfig('institutionsUrl') + filter;
+        return dataService.getData(url);
+    }
+
+    return {
+        getRespondents: getRespondents
+    };
+}]);
--- src/WebApp/wwwroot/js/services/configService.js
+++ src/WebApp/wwwroot/js/services/configService.js
@@ -2,6 +2,7 @@ app.factory('configService', [function () {
 
     var configurations = [];
     configurations['lookupUrl'] = "https://api.consumerfinance.gov/data/hmda/concept/"
+    configurations['institutionsUrl'] = "https://api.consumerfinance.gov:443/data/hmda/slice/institutions.json"
     return {
         getConfig:function(what){
             return configurations[what];
